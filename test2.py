bf4d95e1d2ab8f5098f68380afe3e8236e58c6a1
richardliu44 <richardliu44@hotmail.com>
2022-02-21 23:25:26 +0800

    2022/02/21 use branch dev1

--- test2.py
+++ test2.py
@@ -1,12 +1,14 @@
 # test2 file.
 
 
-# 2020/2/20: add something then push to Github.
+# 2022/2/20: add something then push to Github.
 
 
-# 2020/02/20 add sth from Github web.
+# 2022/02/20 add sth from Github web.
 
-# 2020/02/20 add sth then push to Github again.
+# 2022/02/20 add sth then push to Github again.
 
-# 2020/02/20 use git push with git-bash...
+# 2022/02/20 use git push with git-bash...
+
+# 2022/02/21 use branch dev1 to commit.
 
